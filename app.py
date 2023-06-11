5de57ab50c22f88ba3ffbd10542824a571de8ae9
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-06-11 10:32:21 +0700

    add request schema

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi_sqlalchemy import DBSessionMiddleware
-from model import model, tokenizer, generate_story_and_choices
+from schema import *
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 import os
 import requests, uuid, json, os
 from routes import router
-
+import uvicorn
 
 load_dotenv()
 
@@ -24,3 +24,5 @@ app.add_middleware(
 app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 app.include_router(router)
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ schema/schema.py
@@ -0,0 +1,12 @@
+# build a schema using pydantic
+from pydantic import BaseModel
+from datetime import datetime
+
+class RequestRecommendationSchema(BaseModel):
+    budget: int
+    jenis_penggunaan: str 
+    cpu_brand: int = 0 # 0 = Any, 1 = Intel, 2 = AMD
+    monitor: int = 0 # 0 = No, 1 = Yes
+    peripheral: int = 0 # 0 = No, 1 = Yes
+    gpu_brand: int = 0 # 0 = Any, 1 = Nvidia, 2 = AMD, 3 = No GPU
+ 
