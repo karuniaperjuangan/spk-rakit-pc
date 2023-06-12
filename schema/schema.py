7d65f2c40ff3c07a41dd0f6f4eb6b7b272fe6330
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-06-13 00:08:37 +0700

    Update schema.py

--- schema/schema.py
+++ schema/schema.py
@@ -5,8 +5,8 @@ from datetime import datetime
 class RequestRecommendationSchema(BaseModel):
     budget: int
     jenis_penggunaan: str = 'gaming'
-    cpu_brand: str = 'any'
+    cpu_brand: str = 'any' # 'any' = any brand, 'intel' = intel only, 'amd' = amd only
     monitor: int = 0 # 0 = No, 1 = Yes
     peripheral: int = 0 # 0 = No, 1 = Yes
-    gpu_brand: str = 'any'
+    gpu_brand: str = 'any' # 'any' = any brand, 'nvidia' = nvidia only, 'amd' = amd only
  
